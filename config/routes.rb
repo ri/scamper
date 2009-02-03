13c0847cd5aaa82474d6abb98afc2109903fbb8b
Bodaniel Jeanes <me@bjeanes.com>
2009-02-03 21:10:49 +1000

    Re-generated user + session code from restful authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,14 @@
 ActionController::Routing::Routes.draw do |map|
-  map.resources :users
+  
+  map.logout '/logout', :controller => 'sessions', :action => 'destroy'
+  map.login '/login', :controller => 'sessions', :action => 'new'
+  map.register '/register', :controller => 'users', :action => 'create'
+  map.signup '/signup', :controller => 'users', :action => 'new'
 
+  map.resources :users
   map.resource :session
-
-  map.resources :products
-
-  map.resources :users
-
   map.resources :hunts
   
-  map.activate '/activate/:activation_code', :controller => 'users', :action => 'activate', :activation_code => nil
-  map.signup '/signup', :controller => 'users', :action => 'new'
-  map.login '/login', :controller => 'sessions', :action => 'new'
-  map.logout '/logout', :controller => 'sessions', :action => 'destroy'
   map.root :controller => "site", :view => "index"
 
   # The priority is based upon order of creation: first created -> highest priority.
