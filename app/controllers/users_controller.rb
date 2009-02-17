267d4ce7e24d5fa927ee020dd707ac69edba3fe8
ryanpascoe <ryan.pascoe1@gmail.com>
2009-02-17 18:48:38 +1000

    blank

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,4 +25,29 @@ class UsersController < ApplicationController
       render :action => 'new'
     end
   end
+  def show 
+      @user = User.find(params[:id]) 
+      respond_to do |format| 
+      format.html # show.html.erb 
+      format.xml { render :xml => @user } 
+    end 
+  end
+  
+  def index 
+      @users = User.find(:all) 
+      respond_to do |format| 
+      format.html # index.html.erb 
+      format.xml { render :xml => @users } 
+          
+      end 
+  end 
+  
+  def destroy 
+      @user = User.find(params[:id]) 
+      @user.destroy 
+      respond_to do |format| 
+      format.html { redirect_to(users_url) } 
+      format.xml { head :ok } 
+      end 
+  end
 end
