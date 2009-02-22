46b87ec2cb1773155f738707f5f63d554e086ad3
ryanpascoe <ryan.pascoe1@gmail.com>
2009-02-22 23:42:20 +1000

    added additional hunt fields

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -4,11 +4,12 @@
 class ApplicationController < ActionController::Base
   # Be sure to include AuthenticationSystem in Application Controller instead
   include AuthenticatedSystem
+  session :session_key => '_depot_session_id' 
   helper :all # include all helpers, all the time
 
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
-  protect_from_forgery # :secret => 'c0b9fc7a69bf6a2034aeeafc7f55e6d5'
+  protect_from_forgery  :secret => 'c0b9fc7a69bf6a2034aeeafc7f55e6d5'
   
   # See ActionController::Base for details 
   # Uncomment this to filter the contents of submitted sensitive data parameters
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090209100658) do
+ActiveRecord::Schema.define(:version => 20090222132920) do
 
   create_table "hunts", :force => true do |t|
     t.string   "name"
@@ -19,13 +19,8 @@ ActiveRecord::Schema.define(:version => 20090209100658) do
     t.text     "location"
     t.datetime "start"
     t.datetime "end"
-  end
-
-  create_table "products", :force => true do |t|
-    t.text     "description"
-    t.text     "answer"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.string   "subject"
+    t.string   "schoolname"
   end
 
   create_table "questions", :force => true do |t|
@@ -36,6 +31,16 @@ ActiveRecord::Schema.define(:version => 20090209100658) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "sessions", :force => true do |t|
+    t.string   "session_id", :null => false
+    t.text     "data"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
   create_table "users", :force => true do |t|
     t.string   "login",                     :limit => 40
