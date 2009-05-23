9e70aa369af21c57236a0712bee28de406912eb6
Bodaniel Jeanes <me@bjeanes.com>
2009-05-23 17:38:22 +1000

    Fixed user creator field not being set

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,16 +11,20 @@ class UsersController < ApplicationController
  
   def create
     logout_keeping_session!
+
     @user = User.new(params[:user])
+    @user.creator = !!params[:user][:creator]
+  
     success = @user && @user.save
     if success && @user.errors.empty?
+      logger.debug @user.creator?.inspect
             # Protects against session fixation attacks, causes request forgery
       # protection if visitor resubmits an earlier form using back
       # button. Uncomment if you understand the tradeoffs.
       # reset session
       self.current_user = @user # !! now logged in
       redirect_back_or_default('/')
-      flash[:notice] = "Thanks for signing up!  We're sending you an email with your activation code."
+      flash[:notice] = "Thanks for signing up!"
     else
       flash[:error]  = "We couldn't set up that account, sorry.  Please try again, or contact an admin (link is above)."
       render :action => 'new'
