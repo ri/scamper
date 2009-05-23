a6782e91e50251de0cc8517efcb3dfae59a354db
Bodaniel Jeanes <me@bjeanes.com>
2009-05-23 16:39:32 +1000

    Made KML file not need authentication to be publicly accessible by google servers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,17 +2,8 @@
 # Likewise, all the methods added will be available for all controllers.
 
 class ApplicationController < ActionController::Base
-  # Be sure to include AuthenticationSystem in Application Controller instead
   include AuthenticatedSystem
-  session :session_key => '_depot_session_id' 
-  helper :all # include all helpers, all the time
-
-  # See ActionController::RequestForgeryProtection for details
-  # Uncomment the :secret if you're not using the cookie session store
-  protect_from_forgery  :secret => 'c0b9fc7a69bf6a2034aeeafc7f55e6d5'
-  
-  # See ActionController::Base for details 
-  # Uncomment this to filter the contents of submitted sensitive data parameters
-  # from your application log (in this case, all fields with names like "password"). 
+  helper :all
+  protect_from_forgery
   filter_parameter_logging :password
 end
