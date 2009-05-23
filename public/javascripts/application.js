1d099ab861f722f1b5c68cb852328ea93ea7bc2a
Bodaniel Jeanes <me@bjeanes.com>
2009-05-23 17:15:35 +1000

    Added qrcode javascript function

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,23 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+function qrcode(message, width, height, image_id)
+{
+  if (image_id == null) image_id = "qrcode";    
+  var qrcode = document.getElementById(image_id);
+  
+  if(qrcode != null) {
+    if (width  == null) width  = 200;
+    if (height == null) height = 200;
+  
+    var base            = "http://chart.apis.google.com/chart?cht=qr";
+    var escaped_message = escape(message);
+  
+    qrcode.alt         = message;
+    qrcode.src         = base    + 
+                          "&chl=" + escaped_message  +
+                          "&chs=" + width.toString() + 
+                          "x"     + height.toString();
+
+    return true;
+  } 
+
+  return false;
+}
