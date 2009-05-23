7e93afba119cba9af7093cda4800a6af1325294a
Bodaniel Jeanes <me@bjeanes.com>
2009-05-23 17:12:54 +1000

    Added answers migration

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090523033919) do
+ActiveRecord::Schema.define(:version => 20090523071219) do
 
   create_table "answers", :force => true do |t|
     t.integer  "question_id"
