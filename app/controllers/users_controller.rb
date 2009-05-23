9b84f24a7aeca9be4ad1b835887fabfb3d90fbc6
ryanpascoe <ryan.pascoe1@gmail.com>
2009-05-23 17:41:24 +1000

    somethign

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,7 +20,7 @@ class UsersController < ApplicationController
       # reset session
       self.current_user = @user # !! now logged in
       redirect_back_or_default('/')
-      flash[:notice] = "Thanks for signing up! ."
+      flash[:notice] = "Thanks for signing up!"
     else
       flash[:error]  = "We couldn't set up that account, sorry.  Please try again, or contact an admin (link is above)."
       render :action => 'new'
