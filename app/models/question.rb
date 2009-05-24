49cbd760083ded510fdd33d2f2c9c722aaa274d5
Ri Liu <riblah@192-168-1-4.tpgi.com.au>
2009-05-24 14:33:32 +1000

    validation for question and answer tweaking

--- app/models/question.rb
+++ app/models/question.rb
@@ -8,6 +8,10 @@ class Question < ActiveRecord::Base
   has_many :responses, :through => :answers
   
   accepts_nested_attributes_for :answers, :allow_destroy => true
+  validates_presence_of :location, :message => "^Please select a location"
+  validates_presence_of :question, :hunt_id, :correct_answer_id
+  
+  
   
   def answered?(user)
     player_id = Player.find_by_user_id_and_hunt_id(user.id, hunt_id).id
