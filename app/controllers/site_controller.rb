7cbb11e1bfb2068b91bd326c6540b43ba72db43f
Ri Liu <riblah@ri-lius-macbook.(none)>
2009-05-31 00:16:25 +1000

    mobile working

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -2,6 +2,8 @@ class SiteController < ApplicationController
   def index
     if current_user
       redirect_to(user_hunts_path(current_user))
+    elsif is_mobile_device?
+        redirect_to(login_path)
     end
   end
 
